0766a717233e122cca7de76afd0685460c614574
caetano braga <csbraga@senacrs.com.br>
2024-02-12 15:04:08 -0300

    .

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'O INESQUECÍVEL GRE-NAL 407 (5X0)',
+    description:
+      'Com gols de Giuliano, dois de Luan e um de Fernandinho, além de um contra do zagueiro ex-Grêmio Réver, vencemos e presenteamos os papais tricolores!',
+    photoCover: 'https://i.ytimg.com/vi/jFQnvkO3UtE/maxresdefault.jpg',
+  },
+  {
+    id: '2',
+    title: 'Relembre os títulos que Fábio Koff conquistou com o Grêmio',
+    description: 'Relembre os títulos que Fábio Koff conquistou com o Grêmio',
+    photoCover:
+      'https://www.rbsdirect.com.br/imagesrc/15965046.jpg?w=1200&h=630&a=c&version=1575255600',
+  },
+  {
+    id: '3',
+    title: '"Maracanaço", goleadas e sete duelos pela Copa do Brasil',
+    description: '"Maracanaço", goleadas e sete duelos pela Copa do Brasil',
+    photoCover:
+      'https://s2-ge.glbimg.com/_nefilBntp4pownEnsNfUqeyM7o=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2018/l/b/szDdajRbmvaS2C8flS6A/carlos-miguel-1997.jpg',
+  },
+  {
+    id: '4',
+    title: 'Grêmio Campeão da Libertadores 1995',
+    description: 'Grêmio Campeão da Libertadores 1995',
+    photoCover:
+      'https://1.bp.blogspot.com/-dmlB55K8wdQ/YXRd32rxohI/AAAAAAAAHOc/4ePWqpqD4JM4SSIXZH4mnjDT975NufnAgCLcBGAsYHQ/s1140/1995.jpg',
+  },
+];
